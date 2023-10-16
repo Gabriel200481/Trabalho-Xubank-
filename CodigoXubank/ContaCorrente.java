77ed130889fc299bf36db9addb0a5ac339bca1b4
Bolinho <104277004+Gabriel200481@users.noreply.github.com>
2023-10-15 22:19:40 -0300

    Update ContaCorrente.java

--- CodigoXubank/ContaCorrente.java
+++ CodigoXubank/ContaCorrente.java
@@ -7,4 +7,18 @@ public class ContaCorrente extends Conta {
         this.taxaMensal = 20.0;
         this.limiteEspecial = 200.0;
     }
+
+    public void aplicarTaxaMensal() {
+        sacar(taxaMensal);
+    }
+
+    @Override
+    public double sacar(Double valor){
+        double sacado = 0;
+        if(valor<=(this.getSaldo()+limiteEspecial)){
+            sacado = valor;
+            this.setSaldo(this.getSaldo()-valor);
+        }
+        return sacado;
+    }
 }
