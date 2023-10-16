d4675c2675c0751af14554a4aaead8e19b92e6f8
Bolinho <104277004+Gabriel200481@users.noreply.github.com>
2023-10-15 22:20:18 -0300

    Update ContaInvestimento.java

--- CodigoXubank/ContaInvestimento.java
+++ CodigoXubank/ContaInvestimento.java
@@ -1,12 +1,30 @@
-public class ContaInvestimento extends Conta{
+public class ContaInvestimento extends Conta {
     private double rendimentoDiario;
     private double imposto;
     private double taxa;
 
-    public Investimento(Cliente cliente, double rendimentoDiario) {
+    public ContaInvestimento(Cliente cliente, double rendimentoDiario) {
         super(cliente);
-        this.rendimentoDiario = rendimentoDiario;
+        this.rendimentoDiario = 0;
         this.imposto = 0.15;
         this.taxa = 0.015;
     }
+
+    public void calcularRendimentoDiario() {
+        double min = -10, max = 10;
+        double taxaDiaria = (int) (Math.floor(Math.random() * (max - min + 1) + min));
+        double rendimento = getSaldo() * taxaDiaria;
+        setSaldo(getSaldo()+rendimento);
+    }
+
+    @Override
+    public double sacar(Double valor) {
+        double sacado = 0;
+        if (valor <= this.getSaldo()) {
+            this.setSaldo(this.getSaldo() - (valor));
+            sacado = valor - (rendimentoDiario * imposto + rendimentoDiario * taxa);
+        }
+        return sacado;
+    }
+
 }
