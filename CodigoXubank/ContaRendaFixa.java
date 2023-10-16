7671d5baf0eb5cf8be7d7683453084600b3b3c12
Arthur Felipe Lopes Ferreira <106031924+Arthur0Felipe@users.noreply.github.com>
2023-10-15 22:27:11 -0300

    Update ContaRendaFixa.java

--- CodigoXubank/ContaRendaFixa.java
+++ CodigoXubank/ContaRendaFixa.java
@@ -1,10 +1,27 @@
-public class ContaRendaFixa extends Conta{
+public class ContaRendaFixa extends Conta {
     private double rendimentoContratado;
     private double imposto;
 
-    public RendaFixa(Cliente cliente, double rendimentoContratado) {
+    public ContaRendaFixa(Cliente cliente, double rendimentoContratado) {
         super(cliente);
         this.rendimentoContratado = rendimentoContratado;
         this.imposto = 0.15;
     }
+
+    @Override
+    public double sacar(Double valor){
+        double sacado = 0;
+        if (valor <= this.getSaldo()) {
+            this.setSaldo(this.getSaldo() - valor);
+            sacado = valor;
+        }
+        return sacado;
+    }
+
+    public void aplicarRendimento() {
+        double rendimento = getSaldo() * rendimentoContratado;
+        double impostoSobreRendimento = rendimento * imposto;
+        depositar(rendimento - impostoSobreRendimento);
+    }
 }
+
